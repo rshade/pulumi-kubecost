a15f5966593d372adb575aaa625aba039c9b92a3
Richard Shade <rshade98@hotmail.com>
2022-03-30 08:08:45 -0500

    Committing initial run

--- __main__.py
+++ __main__.py
@@ -1,21 +1,26 @@
 """A Kubernetes Python Pulumi program"""
 
 import pulumi
+from pulumi import ResourceOptions
 from pulumi_kubernetes.apps.v1 import Deployment, DeploymentSpecArgs
 from pulumi_kubernetes.meta.v1 import LabelSelectorArgs, ObjectMetaArgs
-from pulumi_kubernetes.core.v1 import ContainerArgs, PodSpecArgs, PodTemplateSpecArgs
+from pulumi_kubernetes.core.v1 import ContainerArgs, PodSpecArgs, PodTemplateSpecArgs, Namespace
+from pulumi_kubernetes.helm.v3 import Chart, ChartOpts
 
-app_labels = { "app": "nginx" }
-
-deployment = Deployment(
-    "nginx",
-    spec=DeploymentSpecArgs(
-        selector=LabelSelectorArgs(match_labels=app_labels),
-        replicas=1,
-        template=PodTemplateSpecArgs(
-            metadata=ObjectMetaArgs(labels=app_labels),
-            spec=PodSpecArgs(containers=[ContainerArgs(name="nginx", image="nginx")])
-        ),
-    ))
-
-pulumi.export("name", deployment.metadata["name"])
+# Create Kubecost Namespace: kubectl create namespace kubecost
+kubecostNameSpace = Namespace("kubecost")
+# Deploy kubecost from yaml
+#helm repo add kubecost https://kubecost.github.io/cost-analyzer/
+#helm install kubecost kubecost/cost-analyzer --namespace kubecost --set kubecostToken="cmljaGFyZC5zaGFkZUBnbWFpbC5jb20=xm343yadf98"
+kubecost = Chart(
+  'kubecost',
+  ChartOpts(
+    fetch_opts={'repo': 'https://kubecost.github.io/cost-analyzer/'},
+    chart='cost-analyzer',
+    namespace= kubecostNameSpace._name,
+    values={
+      "kubecostToken": "cmljaGFyZC5zaGFkZUBnbWFpbC5jb20=xm343yadf98"
+    }
+  ),
+  opts=ResourceOptions(depends_on=[kubecostNameSpace])
+)
